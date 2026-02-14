ef4ff7b8531d5f2f714c5dc897620f1b085807f2
alwaysvivek <vivekdagar@zohomail.in>
2026-02-14 17:58:58 +0530

    remove create_db.py, scrub hardcoded db password

--- backend/database.py
+++ backend/database.py
@@ -2,8 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# TODO: move to env var before deploying
-DB_URL = "postgresql://postgres:02042007@localhost/event_db"
+import os
+
+# reads from env so we don't commit passwords to git
+DB_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost/event_db")
 
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
